78bdf40dab0fa798c0dab7e5477fc318ba9a9279
John Rovie R. Balingbing <rvc.blngbng@gmail.com>
2024-02-03 18:20:06 +0800

    Added NavBar in the component folder

--- online-portfolio/src/App.js
+++ online-portfolio/src/App.js
@@ -1,9 +1,9 @@
-
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold font-signature">HELLO!</h1>
+      <NavBar />
     </div>
   );
 }
